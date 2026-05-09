dad16d30eab5c525eb2a36c3b5d9dd7803ef2512
js-das-ist-pb <paulpb0725@gmail.com>
2026-05-09 12:41:26 +0800

    idk

---
+++ script.js
@@ -0,0 +1,5 @@
+const settopic = document.getElementById("settopic")
+const gametime = document.getElementById("gametime")
+const gametimeval = document.getElementById("gametimeval")
+const setting = document.getElementById("setting")
+
